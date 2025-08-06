66e871479c4a6530e2d394876cf7fb732b96d68f
thlee <lmnxyz271@gmail.com>
2025-08-06 13:40:23 +0900

    FastApiServiceImpl 삭제

--- ai/src/main/java/forbee/domain/FastApiService.java
+++ ai/src/main/java/forbee/domain/FastApiService.java
@@ -1,5 +1,12 @@
 package forbee.domain;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "fast-api", url = "${fastapi.url}")
 public interface FastApiService {
+    
+    @PostMapping("/object-detection")
     void requestAnalysis(ImageAnalysisRequest request);
 }
