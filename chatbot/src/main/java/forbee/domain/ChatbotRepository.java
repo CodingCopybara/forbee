f96c3f035c618582ef97504ce12ca6727f7af2a7
MiSo Choi <chlalth0501@naver.com>
2025-08-07 00:25:37 +0000

    머지 전 단계

--- chatbot/src/main/java/forbee/domain/ChatbotRepository.java
+++
@@ -1,8 +0,0 @@
-package forbee.domain;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatbotRepository extends JpaRepository<Chatbot, Long> {
-    List<Chatbot> findByUserIdOrderByRequestTimeAsc(String userId);
-}
