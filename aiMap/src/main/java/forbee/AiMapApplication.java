e11694160684dcea23392c0256a83d39d73593f6
JaeSeung Lee <fndl5759@gmail.com>
2025-08-07 01:27:09 +0000

    fix: aiMap 오류 파일 수정

--- aiMap/src/main/java/forbee/AiMapApplication.java
+++ aiMap/src/main/java/forbee/AiMapApplication.java
@@ -2,7 +2,6 @@ package forbee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD:aiMap/src/main/java/forbee/AiMapApplication.java
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import forbee.config.kafka.KafkaProcessor;
 
@@ -12,12 +11,5 @@ public class AiMapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AiMapApplication.class, args);
-=======
-
-@SpringBootApplication
-public class CommunityApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CommunityApplication.class, args);
->>>>>>> origin/feat/frontend/community:community/src/main/java/forbee/CommunityApplication.java
     }
 }
