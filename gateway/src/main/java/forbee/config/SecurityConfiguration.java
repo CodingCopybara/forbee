fe449e072f01ee1a389aa0a1940f6b3a6ea6db51
MiSo Choi <chlalth0501@naver.com>
2025-08-08 06:28:18 +0000

    댓글 db 문제 해결

--- gateway/src/main/java/forbee/config/SecurityConfiguration.java
+++ gateway/src/main/java/forbee/config/SecurityConfiguration.java
@@ -20,7 +20,7 @@ public class SecurityConfiguration {
         http
             .csrf().disable()
             .authorizeExchange()
-            .pathMatchers("/login/**", "/logout**", "/products/**", "/api/users/register", "/oauth/token", "/users/**", "/posts/**")
+            .pathMatchers("/login/**", "/logout**", "/products/**", "/api/users/register", "/oauth/token", "/users/**", "/posts/**", "/comments/**")
             .permitAll()
             .anyExchange()
             .authenticated()
