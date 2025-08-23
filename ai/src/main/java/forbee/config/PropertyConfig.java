90786ef7a1e83a5764c4bbd9210e3450ec0c9edc
thlee17 l <lmnxyz271@gmail.com>
2025-08-23 12:03:48 +0000

    gitpod ai 실행 수정

---
+++ ai/src/main/java/forbee/config/PropertyConfig.java
@@ -0,0 +1,13 @@
+package forbee.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:env.properties")
+})
+public class PropertyConfig {
+
+}
